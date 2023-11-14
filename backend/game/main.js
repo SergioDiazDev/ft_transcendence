e4171619823c9fea08410ac8f4cb116794cf060a
emartinez <enri.fc.98@gmail.com>
2023-11-14 23:16:36 +0100

    refactor: now the game and the scene are the same

--- backend/game/main.js
+++ backend/game/main.js
@@ -1,103 +1,31 @@
-import * as THREE from 'three';
-import WebGL from 'three/addons/capabilities/WebGL.js'
-import Game from './objects.js';
-
-function create_background(game) {
-	let geometry = new THREE.PlaneGeometry(game.width, game.height);
-	let material = new THREE.MeshStandardMaterial(
-			{ color: 0x202646, roughness: 0.1, metalness: 0}
-		);
-	let cube = new THREE.Mesh(geometry, material);
-	cube.position.set(game.width / 2, game.height / 2, 0);
-	return cube;
-}
-
-function create_ball(ball) {
-	let geometry = new THREE.SphereGeometry(ball.size, ball.size * 3, 16 ); 
-	let material = new THREE.MeshStandardMaterial( { color: 0xffffff} ); 
-	let sphere = new THREE.Mesh( geometry, material ); 
-	sphere.position.set(ball.pos.x, ball.pos.y, 5);
-	return sphere;
-}
-
-function create_paddle(paddle) {
-	let geometry = new THREE.CapsuleGeometry(paddle.width, paddle.height - paddle.width, 2, 10);
-	let material = new THREE.MeshPhongMaterial( { color: paddle.color, shininess: 80 } );
-	let cube = new THREE.Mesh(geometry, material);
-	cube.position.set(paddle.pos.x, paddle.pos.y, 5);
-	return cube;
-}
-
-function create_light() {
-	let light = new THREE.PointLight( 0xffffff, 1, 1000, 0);
-	return light;
-}
-
-class GameScene extends THREE.Scene {
-	constructor () {
-		super();
-		this.game = new Game();
-		this.width = this.game.width * 10;
-		this.height = this.game.height * 10;
-		this.renderer = new THREE.WebGLRenderer({antialias: true});
-		this.camera = new THREE.PerspectiveCamera(40, this.game.width / this.game.height, 0.1, 1000);
-		this.camera.position.set(this.game.width / 2, this.game.height / 2, 100);
-		this.render();
-	}
-	render() {
-		this.renderer.setSize(this.width, this.height);
-		// esta clase es la escena, asi que podemos hacer this.add de elementos
-		this.sphere = create_ball(this.game.ball); 
-		this.paddle1 = create_paddle(this.game.paddle1);
-		this.paddle2 = create_paddle(this.game.paddle2);
-		this.background = create_background(this.game);
-		this.light = create_light();
-		this.light.position.set(this.game.width / 2, this.game.height / 2, 100);
-		this.light.target = this.sphere;
-		this.add(this.sphere);
-		this.add(this.paddle1);
-		this.add(this.paddle2);
-		this.add(this.background);
-		this.add(this.light);
-		document.body.appendChild(this.renderer.domElement);
-	}
-	update() {
-		this.game.update();
-		this.sphere.position.x = this.game.ball_pos.x;
-		this.sphere.position.y = this.game.ball_pos.y;
-		this.paddle1.position.x = this.game.paddle1.pos.x;
-		this.paddle1.position.y = this.game.paddle1.pos.y;
-		this.paddle2.position.x = this.game.paddle2.pos.x;
-		this.paddle2.position.y = this.game.paddle2.pos.y;
-	}
-}
+import * as THREE from "three";
+import WebGL from "three/addons/capabilities/WebGL.js";
+import Game from "./objects.js";
 
 // Add event listener on keydown
-document.addEventListener('keydown', (event) => {
-  var name = event.key;
-	if (name == "ArrowDown")
-		scene.game.paddle2.moveDown();
-	else if (name == "ArrowUp")
-		scene.game.paddle2.moveUp();
-	else if (name == "s")
-		scene.game.paddle1.moveDown();
-	else if (name == "w")
-		scene.game.paddle1.moveUp();
-}, false);
+document.addEventListener(
+	"keydown",
+	(event) => {
+		var name = event.key;
+		if (name == "ArrowDown") scene.pad1.moveDown();
+		if (name == "ArrowUp") scene.pad1.moveUp();
+		if (name == "s") scene.pad2.moveDown();
+		if (name == "w") scene.pad2.moveUp();
+	},
+	false,
+);
 
 /* three.js variables */
-let scene = new GameScene();
+let scene = new Game();
 let frames = 0;
 let prevTime = performance.now();
 
-function animate()
-{
+function animate() {
 	requestAnimationFrame(animate);
 	scene.update();
 	frames++;
 	const time = performance.now();
-	if (time >= prevTime + 1000) 
-	{
+	if (time >= prevTime + 1000) {
 		console.log("FPS:", Math.round((frames * 1000) / (time - prevTime)));
 		prevTime = time;
 		frames = 0;
@@ -105,10 +33,8 @@ function animate()
 	scene.renderer.render(scene, scene.camera);
 }
 
-if (WebGL.isWebGLAvailable())
-	animate();
-else
-{
+if (WebGL.isWebGLAvailable()) animate();
+else {
 	const warning = WebGL.getWebGLErrorMessage();
 	document.getElementsByName("body").appendChild(warning);
 }
--- backend/game/objects.js
+++ backend/game/objects.js
@@ -1,139 +1,199 @@
 /* game logic */
 
-const GAME_WIDTH = 100;
-const GAME_HEIGHT = GAME_WIDTH / 16 * 9;
+import * as THREE from "three";
 
-const BALL_SPEED = 1;
+const GAME_WIDTH = 100;
+const GAME_HEIGHT = (GAME_WIDTH / 16) * 9;
+
+const BALL_SPEED = 0.5;
+const OBJECTS_Z = 0;
 
 const PAD_H = GAME_HEIGHT / 5;
 const PAD_W = GAME_WIDTH / 100;
+const PAD_OFFSET_X = PAD_W * 5;
 const PAD_INITIAL_Y = GAME_HEIGHT / 2;
-const PAD_SPEED = PAD_H / 10;
+const PAD_SPEED = 0.5;
+const BALL_SIZE = GAME_WIDTH / 100;
 
 const MAX_SCORE = 5;
 
-class Paddle
-{
-	constructor(x, color) {
-		this.initial_pos = {"x": x, "y": PAD_INITIAL_Y};
-		this.pos = {"x": x, "y": PAD_INITIAL_Y};
+function half(value) {
+	return value / 2.0;
+}
+
+function create_light() {
+	let light = new THREE.PointLight(0xffffff, 1, 1000, 0);
+	return light;
+}
+
+class Paddle extends THREE.Mesh {
+	constructor(x, color, index) {
+		var capsule_length = PAD_H - PAD_W * 3;
+		super(
+			new THREE.CapsuleGeometry(PAD_W, capsule_length, 3, 10),
+			new THREE.MeshPhongMaterial({ color: color, shininess: 80 }),
+		);
+		this.index = index;
+		this.position.set(x, PAD_INITIAL_Y, OBJECTS_Z);
+		this.initial_pos = new THREE.Vector3(x, PAD_INITIAL_Y, OBJECTS_Z);
 		this.color = color;
 		this.width = PAD_W;
 		this.height = PAD_H;
 	}
 	moveUp() {
-		if (this.pos.y < GAME_HEIGHT - PAD_H / 2)
-			this.pos.y += PAD_SPEED;
+		if (this.position.y < GAME_HEIGHT - half(PAD_H)) {
+			this.position.y += PAD_SPEED;
+		}
 	}
 	moveDown() {
-		if (this.pos.y > 0 + PAD_H / 2)
-			this.pos.y -= PAD_SPEED;
+		if (this.position.y > 0 + half(PAD_H)) {
+			this.position.y -= PAD_SPEED;
+		}
 	}
 }
 
-class Ball
-{
+class Ball extends THREE.Mesh {
 	constructor() {
-		this.pos = {"x": GAME_WIDTH / 2, "y": GAME_HEIGHT / 2};
-		this.dir = {"x": BALL_SPEED, "y": 0};
-		this.size = GAME_WIDTH / 100;
+		const radius = BALL_SIZE;
+		super(
+			new THREE.SphereGeometry(radius, 32, 16),
+			new THREE.MeshStandardMaterial({ color: 0x00ff00 }),
+		);
+		this.radius = radius;
+		this.position.set(half(GAME_WIDTH), half(GAME_HEIGHT), OBJECTS_Z);
+		this.direction = new THREE.Vector3(BALL_SPEED, 0, 0);
 	}
-	move()
-	{
-		this.pos.x += this.dir.x;
-		this.pos.y += this.dir.y;
+	move() {
+		this.position.x += this.direction.x;
+		this.position.y += this.direction.y;
+	}
+	get lower_y() {
+		return this.position.y - this.radius;
+	}
+	get upper_y() {
+		return this.position.y + this.radius;
+	}
+	get left_x() {
+		return this.position.x - this.radius;
+	}
+	get right_x() {
+		return this.position.x + this.radius;
 	}
 }
 
-class Game
-{
+class Board extends THREE.Mesh {
 	constructor() {
-		this.score = {"p1": 0, "p2": 0}
+		super(
+			new THREE.PlaneGeometry(GAME_WIDTH, GAME_HEIGHT),
+			new THREE.MeshStandardMaterial({
+				color: 0x202646,
+				roughness: 0.1,
+				metalness: 0,
+			}),
+		);
+		this.position.set(half(GAME_WIDTH), half(GAME_HEIGHT), 0);
+	}
+}
+
+class Game extends THREE.Scene {
+	constructor() {
+		super();
+		this.score = { p1: 0, p2: 0 };
 		this.width = GAME_WIDTH;
 		this.height = GAME_HEIGHT;
-		var offset_x = PAD_W * 5;
-		this.paddle1 = new Paddle(0 + offset_x, 0xff0000);
-		this.paddle2 = new Paddle(GAME_WIDTH - offset_x, 0x0000ff);
+		this.board = new Board();
+		this.pad1 = new Paddle(0 + PAD_OFFSET_X, 0xff0000, 1);
+		this.pad2 = new Paddle(GAME_WIDTH - PAD_OFFSET_X, 0x0000ff, 2);
 		this.ball = new Ball();
+		this.renderer = new THREE.WebGLRenderer({ antialias: true });
+		this.camera = new THREE.PerspectiveCamera(40, this.width / this.height);
+		this.camera.position.set(half(this.width), half(this.height), 100);
+		this.render();
+	}
+	render() {
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		// esta clase es la escena, asi que podemos hacer this.add de elementos
+		this.light = create_light();
+		this.light.position.set(this.width / 2, this.height / 2, 100);
+		this.light.target = this.sphere;
+		this.add(this.ball);
+		this.add(this.pad1);
+		this.add(this.pad2);
+		this.add(this.board);
+		this.add(this.light);
+		document.body.appendChild(this.renderer.domElement);
 	}
 	update() {
-		if (!this.check_game_end())
-		{
+		if (!this.check_game_end()) {
 			var goal = this.check_goal();
-			if (goal != 0)
-				this.reset_board(goal);
-			var pad_hit = this.check_pad_collision();
-			if (pad_hit != 0)
-				this.calculate_ball_dir(pad_hit);
-			if (this.check_board_collision())
-				this.ball.dir.y = -this.ball.dir.y;
+			if (goal != 0) this.reset_board(goal);
+			var pad_hit = this.check_pad_collisions();
+			if (pad_hit != 0) this.calculate_ball_dir(pad_hit);
+			if (this.check_board_collision()) this.ball.direction.y *= -1;
 			this.ball.move();
 		}
 	}
 	reset_board(direction) {
-		this.ball.pos = {"x": GAME_WIDTH / 2, "y": GAME_HEIGHT / 2};
-		this.ball.dir = {"x": direction * BALL_SPEED, "y": 0};
-		this.paddle1.pos.x = this.paddle1.initial_pos.x;
-		this.paddle1.pos.y = this.paddle1.initial_pos.y;
-		this.paddle2.pos.x = this.paddle2.initial_pos.x;
-		this.paddle2.pos.y = this.paddle2.initial_pos.y;
-	}
-	get ball_pos() {
-		return this.ball.pos;
-	}
-	get pad1_pos() {
-		return this.paddle1.pos
-	}
-	get pad2_pos() {
-		return this.paddle2.pos;
+		this.ball.position.set(half(GAME_WIDTH), half(GAME_HEIGHT), OBJECTS_Z);
+		this.ball.direction.set(direction * BALL_SPEED, 0, 0);
+		this.pad1.position.setY(this.pad1.initial_pos.y);
+		this.pad2.position.setY(this.pad2.initial_pos.y);
 	}
 	check_goal() {
-		if (this.ball.pos.x > GAME_WIDTH)
-		{
+		if (this.ball.position.x > GAME_WIDTH) {
 			this.score.p1 += 1;
 			console.log("Player 1:", this.score.p1, "Player 2:", this.score.p2);
-			return (1);
-		}
-		else if (this.ball.pos.x < 0)
-		{
+			return 1;
+		} else if (this.ball.position.x < 0) {
 			this.score.p2 += 1;
 			console.log("Player 1:", this.score.p1, "Player 2:", this.score.p2);
-			return (-1);
+			return -1;
 		}
-		return (0);
+		return 0;
 	}
-	check_pad_collision() {
-		var ball_x = this.ball_pos.x;
-		var ball_y = this.ball_pos.y;
-
-		if (ball_x >= this.pad1_pos.x - PAD_W / 2 && ball_x <= this.pad1_pos.x + PAD_W / 2 && 
-			ball_y >= this.pad1_pos.y - PAD_H / 2 && ball_y <= this.pad1_pos.y + PAD_H / 2)
-			return (-1);
-		if (ball_x >= this.pad2_pos.x - PAD_W / 2 && ball_x <= this.pad2_pos.x + PAD_W / 2 && 
-			ball_y >= this.pad2_pos.y - PAD_H / 2 && ball_y <= this.pad2_pos.y + PAD_H / 2)
-			return (1);
-		return (0);
+	check_pad_collisions() {
+		if (this.calculate_pad_hit(this.ball, this.pad1)) return -1;
+		if (this.calculate_pad_hit(this.ball, this.pad2)) return 1;
+		return 0;
+	}
+	calculate_pad_hit(ball, pad) {
+		var hit = {
+			x: ball.right_x - pad.position.x,
+			y: ball.position.y - pad.position.y,
+		};
+		if (pad.index == 1) hit.x = ball.left_x - pad.position.x;
+		if (
+			hit.y <= half(PAD_H) &&
+			hit.y >= half(-PAD_H) &&
+			hit.x <= half(PAD_W) &&
+			hit.x >= half(-PAD_W)
+		)
+			return true;
+		return false;
 	}
 	check_board_collision() {
-		if (this.ball_pos.y <= 0 || this.ball_pos.y >= GAME_HEIGHT)
-			return (true);
-		return (false);
+		if (this.ball.lower_y <= 0 || this.ball.upper_y >= GAME_HEIGHT)
+			return true;
+		return false;
 	}
 	calculate_ball_dir(pad_hit) {
-		if (pad_hit == -1)
-			this.ball.dir = {"x": -this.ball.dir.x,
-							 "y": (this.ball_pos.y - this.pad1_pos.y) / PAD_H};
-		if (pad_hit == 1)
-			this.ball.dir = {"x": -this.ball.dir.x,
-							 "y": (this.ball_pos.y - this.pad2_pos.y) / PAD_H};
+		if (pad_hit == -1) {
+			this.ball.direction.x *= -1;
+			this.ball.direction.y =
+				(this.ball.position.y - this.pad1.position.y) / PAD_H;
+		}
+		if (pad_hit == 1) {
+			this.ball.direction.x *= -1;
+			this.ball.direction.y =
+				(this.ball.position.y - this.pad2.position.y) / PAD_H;
+		}
 	}
 	check_game_end() {
-		if (this.score.p1 >= MAX_SCORE || this.score.p2 >= MAX_SCORE)
-		{
+		if (this.score.p1 >= MAX_SCORE || this.score.p2 >= MAX_SCORE) {
 			console.log("Game ended");
-			return (true);
+			return true;
 		}
-		return (false);
+		return false;
 	}
 }
 
