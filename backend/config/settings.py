a2d034dc56c194a54f465176d2221a423770c7e9
andresja <andresjadx@gmail.com>
2023-12-22 04:37:56 +0100

    PlayerFriend model created inside player_friends app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+
     'corsheaders', # This app is for handling cors
     'rest_framework', # This handles APIs
 
