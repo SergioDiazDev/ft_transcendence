18b2b885d5ab330bd9ca0c9b54a890a56a15576e
emartinez <enri.fc.98@gmail.com>
2024-01-24 18:29:25 +0100

    feat(game): add pong movements and async game logic

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -1,47 +1,97 @@
 import json
+import asyncio
+import uuid
 
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class GameConsumer(WebsocketConsumer):
-	def connect(self):
+class GameConsumer(AsyncWebsocketConsumer):
+	players = {}
+	update_lock = asyncio.Lock()
+
+	async def connect(self):
 		# each consumer has a scope that contains info about its connection, like game_id in the URL
 		# or the currently authenticated user
-		self.user = self.scope["user"]
+
+		# uncomment this line when users are implemented
+		# self.user = self.scope["user"]
+		self.user = str(uuid.uuid1())
+		await self.accept()
+
 		self.room_name = self.scope["url_route"]["kwargs"]["game_id"] 
 		# construct a channel group based on the game_id (should be unique)
 		self.room_group_name = f"game_{self.room_name}"
 
+		# accept the websocket connection, if we remove this, the user will be rejected,
+		# its useful for example for authenticating users
+
 		# the async_to_sync wrapper is required because GameConsumer
 		# is synchronous but is calling a Channel (all Channel are asynchronous)
 		# group_add -> join the group
-		async_to_sync(self.channel_layer.group_add)(
+		await self.channel_layer.group_add(
 			self.room_group_name, self.channel_name
 		)
-		# accept the websocket connection, if we remove this, the user will be rejected,
-		# its useful for example for authenticating users
-		self.accept()
 
-	def disconnect(self, close_code):
+		await self.send(
+            text_data=json.dumps({"type": "player_join", "player_id": self.user})
+        )
+
+		async with self.update_lock:
+			self.players[self.user] = {
+				"id": self.user,
+				"board_pos": len(self.players),
+				"direction": 0,
+				"y_pos": 0,
+			}
+
+		if len(self.players) == 2:
+			asyncio.create_task(self.game_loop())
+
+	async def disconnect(self, close_code):
+		async with self.update_lock:
+			if self.user in self.players:
+				del self.players[self.user]
+
 		# group_discard -> disconnect channel from the group
-		async_to_sync(self.channel_layer.group_discard)(
+		await self.channel_layer.group_discard(
 			self.room_group_name, self.channel_name
 		)
 
-	def receive(self, text_data):
+	async def receive(self, text_data):
 		text_data_json = json.loads(text_data)
 		movement = text_data_json["movement"]
+		player_id = text_data_json["player"]
 
-		# group_send -> sends an event to a group
-		# events have special 'type' key, corresponding with the name of the method that should be
-		# invoked on consumers, replacing game.movement with game_movement, calling the method below
-		async_to_sync(self.channel_layer.group_send)(
-			self.room_group_name, {"type": "game.movement", "movement": movement, "player": self.user}
+		player = self.players.get(player_id, None)
+		if not player:
+			return
+
+		if movement == "up":
+			player["direction"] = 1
+		elif movement == "down":
+			player["direction"] = -1
+		else:
+			player["direction"] = 0
+
+	async def gamestate_update(self, event):
+		await self.send(
+			text_data = json.dumps(
+				{
+					"type": "gamestate_update",
+					"objects": event["objects"],
+				}
+			)
 		)
 
-	def game_movement(self, event):
-		movement = event["movement"]
-		player = event["player"]
-		if not player.is_authenticated:
-			player = "Anonymous player"
-		self.send(text_data=json.dumps({"movement": movement, "player": player}))
+	async def game_loop(self):
+		while len(self.players) > 0:
+			async with self.update_lock:
+				for player in self.players.values():
+					# game logic
+					pass
+
+			await self.channel_layer.group_send(
+				self.room_group_name,
+				{"type": "gamestate_update", "objects": list(self.players.values())},
+			)
+			await asyncio.sleep(0.05)
