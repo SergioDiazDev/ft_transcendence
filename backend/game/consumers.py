c021c2899bd35af0c9fb294fc9f427e4e106ad90
emartinez <enri.fc.98@gmail.com>
2024-01-25 13:10:12 +0100

    feat(game): implement rooms, but they are all the same idk why

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -1,3 +1,4 @@
+import os
 import json
 import asyncio
 import uuid
@@ -7,56 +8,55 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from game.game_logic import PongGame
 
 class GameConsumer(AsyncWebsocketConsumer):
-	players = {}
-	game = PongGame()
+	rooms = {}
 	update_lock = asyncio.Lock()
 
 	async def connect(self):
 		# each consumer has a scope that contains info about its connection, like game_id in the URL
 		# or the currently authenticated user
-
 		# uncomment this line when users are implemented
 		# self.user = self.scope["user"]
+
 		self.user = str(uuid.uuid1())
-		async with self.update_lock:
-			if len(self.players) == 2:
-				return
 		await self.accept()
 
 		self.room_name = self.scope["url_route"]["kwargs"]["game_id"] 
 		# construct a channel group based on the game_id (should be unique)
 		self.room_group_name = f"game_{self.room_name}"
 
-		# accept the websocket connection, if we remove this, the user will be rejected,
-		# its useful for example for authenticating users
-
-		# the async_to_sync wrapper is required because GameConsumer
-		# is synchronous but is calling a Channel (all Channel are asynchronous)
-		# group_add -> join the group
 		await self.channel_layer.group_add(
 			self.room_group_name, self.channel_name
 		)
+
+		print(f"Se ha unido el usuario {self.user} con el canal {self.channel_layer} al grupo {self.room_group_name}", os.getcwd(), flush=True)
 
 		await self.send(
             text_data=json.dumps({"type": "player_join", "player_id": self.user})
         )
 
 		async with self.update_lock:
-			self.players[self.user] = {
-				"id": self.user,
-				"board_pos": len(self.players),
-				"direction": 0,
-			}
+			if self.room_group_name in self.rooms.keys():
+				self.rooms[self.room_group_name]["players"][self.user] = {
+					"id": self.user,
+					"board_pos": len(self.rooms[self.room_group_name]["players"]),
+					"direction": 0,
+				}
+			else:
+				self.rooms[self.room_group_name] = {"players": {self.user: {}}}
+				self.rooms[self.room_group_name]["players"][self.user] = {
+					"id": self.user,
+					"board_pos": 0,
+					"direction": 0,
+				}
 
-		if len(self.players) == 2:
-			asyncio.create_task(self.game_loop())
+			if len(self.rooms[self.room_group_name]["players"]) == 2:
+				self.rooms[self.room_group_name]["game"] = PongGame()
+				asyncio.create_task(self.game_loop())
 
 	async def disconnect(self, close_code):
 		async with self.update_lock:
-			if self.user in self.players:
-				del self.players[self.user]
-			if len(self.players) == 0:
-				self.game = PongGame()
+			if self.user in self.rooms[self.room_group_name]["players"]:
+				del self.rooms[self.room_group_name]["players"][self.user]
 
 		# group_discard -> disconnect channel from the group
 		await self.channel_layer.group_discard(
@@ -68,7 +68,7 @@ class GameConsumer(AsyncWebsocketConsumer):
 		movement = text_data_json["movement"]
 		player_id = text_data_json["player"]
 
-		player = self.players.get(player_id, None)
+		player = self.rooms[self.room_group_name]["players"].get(player_id, None)
 		if not player:
 			return
 
@@ -84,24 +84,31 @@ class GameConsumer(AsyncWebsocketConsumer):
 			text_data = json.dumps(
 				{
 					"type": "gamestate_update",
+					"room": event["room"],
 					"objects": event["objects"],
-					"gamestate": self.game.get_gamestate(),
+					"gamestate": event["gamestate"],
 				}
 			)
 		)
 
 	async def game_loop(self):
-		while len(self.players) == 2:
-			async with self.update_lock:
-				for player in self.players.values():
+		print(f"Game loop for room {self.room_group_name}", os.getcwd(), flush=True)
+		#print("Rooms\n", self.rooms, "\n", os.getcwd(), flush=True)
+		room = self.rooms.get(self.room_group_name, None)
+		if room:
+			#print(room, os.getcwd(), flush=True)
+			while len(room["players"]) == 2:
+				for player_id in room["players"]:
+					player = room["players"][player_id]
+					#print(player, os.getcwd(), flush=True)
 					if player["board_pos"] == 0:
-						self.game.pad1.move(player["direction"])
+						room["game"].pad1.move(player["direction"])
 					elif player["board_pos"] == 1:
-						self.game.pad2.move(player["direction"])
-				self.game.calculate_frame()
-				await asyncio.sleep(0.016)
+						room["game"].pad2.move(player["direction"])
+				room["game"].calculate_frame()
 
-			await self.channel_layer.group_send(
-				self.room_group_name,
-				{"type": "gamestate_update", "objects": list(self.players.values()), "gamestate": self.game.get_gamestate()},
-			)
+				await self.channel_layer.group_send(
+					self.room_group_name,
+					{"type": "gamestate_update", "room": self.room_group_name, "objects": list(room["players"]), "gamestate": room["game"].get_gamestate()},
+				)
+				await asyncio.sleep(0.016)
