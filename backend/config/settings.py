20f6a8e83c9c53103530c6fbe9595c7b13fdac2c
emartinez <enri.fc.98@gmail.com>
2024-01-29 11:08:18 +0100

    fix: host got overwritten on merge

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -114,7 +114,7 @@ DATABASES = {
         'NAME': os.getenv("POSTGRES_DB_NAME"),
         'USER': os.getenv("POSTGRES_USER"),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-        'HOST': '127.0.0.1',
+        'HOST': 'postgresql',
         'PORT': '5432'
     }
 }
