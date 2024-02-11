fcf48273d6b370ff8ee7d5ac1bb6e92daa74871d
Sergio Diaz <SergioDiazDev@gmail.com>
2024-02-11 20:50:20 +0100

    Create Template Friends

--- backend/player_friends/views.py
+++ backend/player_friends/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 
 from .models import PlayerFriend
+from accounts.models import Player
 
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def showFriends(request):
 	friends = PlayerFriend.objects.filter(myUser=request.user)
-	return render(request, 'friends.html', {'friends': friends})
+
+	users = Player.objects.all()
+
+	find_user = None
+	find = request.GET.get('user')
+	print("find =", request.GET.get('user'))
+	if find:
+		find_user = PlayerFriend.objects.filter(myFriend__username=find).first()
+
+	return render(request, 'friends.html', {'friends': friends, "users": users, "find_user": find_user})
 
 @login_required
 def showAll(request):
-	users = Player.objects.all()
 	return render(request, 'users.html', {'users': users})
 
 @login_required
