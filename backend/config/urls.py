c9dc3743410dc890e07217d8502cdecda8b7406c
Sergio Diaz <SergioDiazDev@gmail.com>
2024-02-11 19:21:09 +0100

    Create Template, Views and Urls for player_Friends

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls'), name='accounts'),
     path('admin/', admin.site.urls),
     path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('friends/', include('player_friends.urls'), name='friends'),
     path('token/', include('tokens.urls')),
     path('game/', include("game.urls")),
     path('game_ai/', include("game.urls")),
--- backend/player_friends/views.py
+++ backend/player_friends/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import PlayerFriend
+
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def showFriends(request):
+	friends = PlayerFriend.objects.filter(myUser=request.user)
+	return render(request, 'friends.html', {'friends': friends})
+
+@login_required
+def showAll(request):
+	users = Player.objects.all()
+	return render(request, 'users.html', {'users': users})
+
+@login_required
+def findUser(request):
+	find = request.GET.get('user')
+	if find:
+		user = PlayerFriend.objects.filter(username = find)
+	else:
+		user = None
+	return render(request, 'finduser.html', {'user': user})
