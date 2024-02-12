9366812532771aec4f2a9b360196ca05484d96e4
Sergio Diaz <SergioDiazDev@gmail.com>
2024-02-12 12:08:24 +0100

    Add Active User

--- backend/player_friends/views.py
+++ backend/player_friends/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from .models import PlayerFriend
 from accounts.models import Player
 
+from datetime import datetime, timedelta, timezone
+
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -16,11 +18,23 @@ def showFriends(request):
 	print("find =", request.GET.get('user'))
 	if find:
 		find_user = PlayerFriend.objects.filter(myFriend__username=find).first()
+	
+	now_utc = datetime.now(timezone.utc)
+
+	# Definir el límite de una hora atrás en UTC
+	one_hour_ago_utc = now_utc - timedelta(hours=1)
+
+	for user in users:
+		if user.last_login and user.last_login.replace(tzinfo=timezone.utc) > one_hour_ago_utc:
+			user.isactive = True
+		else:
+			user.isactive = False
 
 	return render(request, 'friends.html', {'friends': friends, "users": users, "find_user": find_user})
 
 @login_required
 def showAll(request):
+	users = Player.objects.all()
 	return render(request, 'users.html', {'users': users})
 
 @login_required
@@ -31,3 +45,13 @@ def findUser(request):
 	else:
 		user = None
 	return render(request, 'finduser.html', {'user': user})
+
+@login_required
+def isactive(request):
+	users = Player.objects.all()
+	for user in users:
+		if (user.last_login > timezone.now() - timedelta(minutes=5)):
+			user.isactive = True
+		else:
+			user.isactive = False
+	return render(request, 'friends.html', {'users': users})
