b187de647039de11ed0063eeb2eb870e95d272d0
andresja <andresjadx@gmail.com>
2024-02-24 14:21:12 +0100

    Getting matchmaking info

--- backend/static/tournament_files/main.js
+++ backend/static/tournament_files/main.js
@@ -2,5 +2,26 @@
 
 window.searchMatch = function()
 {
+    console.log("Starting web socket...");
     const matchSock = new WebSocket(`ws://${window.location.host}/ws/matchmaking/`);
+
+    matchSock.onopen = function(){
+        matchSock.send(JSON.stringify({
+            search: true
+        }));
+    }
+
+    matchSock.onmessage = function(e){
+        const data = JSON.parse(e.data);
+        console.log(Object.keys(data));
+        if (Object.keys(data).includes("info") && data["info"])
+        {
+            console.log("Tiene info...");
+        }
+
+        if(Object.keys(data).includes("close") && data["close"])
+        {
+            matchSock.close();
+        }
+    }
 }
--- backend/tournament/consumers.py
+++ backend/tournament/consumers.py
@@ -1,9 +1,47 @@
+import os
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+from uuid import uuid4
+import json
+
 class MatchmakingConsumer(AsyncWebsocketConsumer):
+    users_searching = []
 
     async def connect(self, *args, **kwargs):
-        self.user = self.scope["user"]
-        print(self.user)
-        await self.accept()
+        if self.scope["user"].is_authenticated:
+            await self.accept()
+        else:
+            await self.close()
+
+    async def disconnect(self, close_code):
+        print("Disconnect", flush = True)
+
+    async def receive(self, text_data):
+        data_json = json.loads(text_data)
+        if data_json["search"]:
+            if len(MatchmakingConsumer.users_searching) <= 0:
+                myuuid = uuid4()
+                MatchmakingConsumer.users_searching.append({str(myuuid): str(self.scope["user"])})
+                self.room_group_name = f"matchmaking-{myuuid}"
+                await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+            elif len(MatchmakingConsumer.users_searching) > 0:
+                keys_list = list(MatchmakingConsumer.users_searching[0].keys())
+                myuuid = keys_list[0]
+                self.room_group_name = f"matchmaking-{myuuid}"
+                await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+                message = {"uuid_game": str(myuuid), "player1": str(MatchmakingConsumer.users_searching[0][myuuid]),
+                    "player2": str(self.scope["user"]), "info": True,  "close": True}
+                await self.channel_layer.group_send(
+                    self.room_group_name, {"type": "matchmaking.message", "message": message}
+                )
+                del MatchmakingConsumer.users_searching[0]
+            
+        print(MatchmakingConsumer.users_searching, flush = True)
+
+
+    async def matchmaking_message(self, event):
+        message = event["message"]
+
+        await self.send(text_data = json.dumps({"message": message}))
