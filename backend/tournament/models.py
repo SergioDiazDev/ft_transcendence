db4be437fe184909e393bd5900b458f36445e7ea
Sergio Diaz <SergioDiazDev@gmail.com>
2024-02-29 01:11:35 +0100

    Tournament models

--- backend/tournament/models.py
+++ backend/tournament/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from game.models import Game
+
+class Tournament(models.Model):
+	id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+	player1 = models.ForeignKey("Player", on_delete=models.DO_NOTHING, related_name = "player1")
+	player2 = models.ForeignKey("Player", on_delete=models.DO_NOTHING, related_name = "player2")
+	player3 = models.ForeignKey("Player", on_delete=models.DO_NOTHING, related_name = "player3")
+	player4 = models.ForeignKey("Player", on_delete=models.DO_NOTHING, related_name = "player4")
+	
+	semiFinal1 = models.ForeignKey("Game", on_delete=models.DO_NOTHING, related_name = "semiFinal1")
+	semiFinal2 = models.ForeignKey("Game", on_delete=models.DO_NOTHING, related_name = "semiFinal2")
+	
+	final = models.ForeignKey("Game", on_delete=models.DO_NOTHING, related_name = "final")
