89317386a4f2993300bee31424e2e02987ef8ab6
abarriga <abarriga4242@gmail.com>
2024-02-29 12:47:23 +0100

    fix: ai URL was on wrong main.js file

--- backend/static/tournament_files/main.js
+++ backend/static/tournament_files/main.js
@@ -1,56 +1,5 @@
 
 
-window.searchMatch = function()
-{
-    // console.log("Starting web socket...");
-    const matchSock = new WebSocket(`wss://${window.location.host}/ws/matchmaking/`);
-    const button_tournament = document.querySelector("#button-tournament")
-
-    matchSock.onopen = function(){
-        matchSock.send(JSON.stringify({
-            search: true
-        }));
-    }
-
-    matchSock.onmessage = function(e){
-        const data = JSON.parse(e.data);
-        if (Object.keys(data["message"]).includes("info") && data["message"]["info"])
-        {
-            let my_user = document.querySelector("#mm-player1").innerText;
-            let enemy_name = null;
-            let uuid_match = data["message"]["uuid_game"];
-
-            if(data["message"]["player1"] == my_user)
-                enemy_name = data["message"]["player2"];
-            else
-                enemy_name = data["message"]["player1"];
-            let avatar_endpoint = `${window.location.protocol}//${window.location.hostname}/accounts/profile/avatar/${enemy_name}/`;
-            // console.log(avatar_endpoint);
-            fetch(avatar_endpoint)
-            .then(raw_data => {
-                return raw_data.json();
-            })
-            .then(({enemy_avatar}) => {
-                document.querySelector("#mm-player2").innerText = enemy_name;
-                document.querySelector("#mm-avatar-player2").src = `${window.location.protocol}//${window.location.hostname}/${enemy_avatar}`;
-                document.querySelector("#loader-tournament").classList.add("no-display");
-                document.querySelector("#mm-enemy-box").classList.toggle("no-display");
-
-                window.blockNavigation = false;
-                button_tournament.setAttribute("href", `/game/${uuid_match}/`)
-            })
-            .catch((error) => console.log(error));
-        }
-
-        if(Object.keys(data["message"]).includes("close") && data["message"]["close"])
-        {
-            matchSock.close();
-            button_tournament.innerText = "Press to continue...";
-        }
-    }
-}
-
-
 //---------------------------- Tournaments --------------------------------
 
 //TODO: Tenemos que hacer que si le damos de nuevo a matchmaking que se cierre si esta abierto
--- backend/static/tournament_files/main2.js
+++ backend/static/tournament_files/main2.js
@@ -1,3 +1,56 @@
+
+
+window.searchMatch = function searchMatch()
+{
+    // console.log("Starting web socket...");
+    const matchSock = new WebSocket(`wss://${window.location.host}/ws/matchmaking/`);
+    const button_tournament = document.querySelector("#button-tournament")
+
+    matchSock.onopen = function(){
+        matchSock.send(JSON.stringify({
+            search: true
+        }));
+    }
+
+    matchSock.onmessage = function(e){
+        const data = JSON.parse(e.data);
+        if (Object.keys(data["message"]).includes("info") && data["message"]["info"])
+        {
+            let my_user = document.querySelector("#mm-player1").innerText;
+            let enemy_name = null;
+            let uuid_match = data["message"]["uuid_game"];
+
+            if(data["message"]["player1"] == my_user)
+                enemy_name = data["message"]["player2"];
+            else
+                enemy_name = data["message"]["player1"];
+            let avatar_endpoint = `${window.location.protocol}//${window.location.hostname}/accounts/profile/avatar/${enemy_name}/`;
+            // console.log(avatar_endpoint);
+            fetch(avatar_endpoint)
+            .then(raw_data => {
+                return raw_data.json();
+            })
+            .then(({enemy_avatar}) => {
+                document.querySelector("#mm-player2").innerText = enemy_name;
+                document.querySelector("#mm-avatar-player2").src = `${window.location.protocol}//${window.location.hostname}/${enemy_avatar}`;
+                document.querySelector("#loader-tournament").classList.add("no-display");
+                document.querySelector("#mm-enemy-box").classList.toggle("no-display");
+
+                window.blockNavigation = false;
+                button_tournament.setAttribute("href", `/game/${uuid_match}/`)
+            })
+            .catch((error) => console.log(error));
+        }
+
+        if(Object.keys(data["message"]).includes("close") && data["message"]["close"])
+        {
+            matchSock.close();
+            button_tournament.innerText = "Press to continue...";
+        }
+    }
+}
+
+
 window.join_tournament = function() {
 		document.querySelector("#tournament-button").remove();
 		document.querySelector("#tournament-display").classList.remove("hidden");
